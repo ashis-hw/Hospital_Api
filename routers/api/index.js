a30248fe5f8644e36d70832c8dea0bfb127c6d84
ASHIS DAS <107852559+ashis-hw@users.noreply.github.com>
2023-07-06 18:24:30 +0530

    Delete routers directory

--- routers/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// set-up routes localhost:3000/api/v1
-router.use('/v1', require('./v1/index'));
-
-module.exports = router;
